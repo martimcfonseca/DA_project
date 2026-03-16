828c1db0efcedc68f6376a4e980c136a2d698ea5
Cristiano Silva <cristianojorge08@gmail.com>
2026-03-16 15:30:52 +0000

    interface basica

---
+++ Menu.h
@@ -0,0 +1,21 @@
+//
+// Created by cristiano on 16/03/26.
+//
+
+#ifndef DA_PROJECT_MENU_H
+#define DA_PROJECT_MENU_H
+
+#include <vector>
+#include <string>
+
+class Menu {
+private:
+    void showmenu();
+public:
+   void run();
+};
+
+
+
+
+#endif //DA_PROJECT_MENU_H
--- Parser.cpp
+++ Parser.cpp
@@ -4,7 +4,9 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <Menu.h>
 
 using namespace std;
 int main() {
+
 }
